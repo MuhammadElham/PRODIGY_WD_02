aed37b869200e5620a3406649cdd12a307ed4e66
Elham <elhamwaheed777@gmail.com>
2025-02-04 04:10:30 +0500

    Checking main branch working fine

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>
-      App
+    <div className='bg-red-700 text-white text-9xl font-bold py-16 text-center'>
+      My App
     </div>
   )
 }
